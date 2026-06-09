eb14f710c526a613d86702a72545a0bd4f0ab8a8
Victor Cristofolini <victorlcristofolini@gmail.com>
2026-06-09 19:34:53 -0300

    SextoCommit

--- Uni07Exe04.java
+++ Uni07Exe04.java
@@ -4,7 +4,11 @@ __ Enunciado:
 
 Dado um texto qualquer faça um programa que, através de subprogramas: a) forneça o número de vogais do texto. 
 b) informe o valor numérico que aparece no início do texto. Informe 0 (zero) se no início não há valor numérico 
-(ver exemplos abaixo): "27C1" - valor numérico = 27 "134" - valor numérico = 134 "XYZ" - valor numérico = 0 "B21" - valor numérico = 0 
+(ver exemplos abaixo): 
+"27C1" - valor numérico = 27 
+"134" - valor numérico = 134 
+"XYZ" - valor numérico = 0 
+"B21" - valor numérico = 0 
 c) forneça a posição de início de uma palavra dentro do texto.
 
 __ Análise: OK
@@ -27,6 +31,12 @@ public class Uni07Exe04 {
         return frase;
     }
 
+     private String lerPalavra (Scanner sc) {
+        System.out.println("Digite a palavra a ser encontrada: ");
+        String palavra = sc.nextLine().toUpperCase();
+        return palavra;
+    }
+
     private int contarVogais(String frase) {
         int contadorVogais = 0;
         for (int i = 0; i < frase.length(); i++) {
@@ -37,6 +47,42 @@ public class Uni07Exe04 {
         }
         return contadorVogais;
     }
+
+    private int informeNumerico(String frase) {
+        int valorNumerico = 0;
+        for (int i = 0; i < frase.length(); i++) {
+            if (Character.isDigit(frase.charAt(i))) {
+                // Character.getNumericValue() converte o caractere numérico para seu valor inteiro correspondente
+                // Multiplicamos o valor atual por 10 para deslocar os dígitos anteriores para a esquerda e adicionamos o novo dígito
+                valorNumerico = valorNumerico * 10 + Character.getNumericValue(frase.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return valorNumerico;
+    }
+
+    private int posicaoInicioPalavra(String texto, String palavra) {
+
+        for (int i = 0; i <= texto.length() - palavra.length(); i++) {
+
+            boolean encontrou = true;
+
+            for (int j = 0; j < palavra.length(); j++) {
+
+                if (texto.charAt(i + j) != palavra.charAt(j)) {
+                    encontrou = false;
+                    break;
+                }
+            }
+
+            if (encontrou) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
     
     
 
@@ -45,8 +91,11 @@ public class Uni07Exe04 {
         String frase = lerFrase(sc);
         int quantidade = contarVogais(frase);
         System.out.println("Quantidade de vogais = " + quantidade);
-
-
+        int valorNumerico = informeNumerico(frase);
+        System.out.println("Valor numérico no início do texto = " + valorNumerico);
+        String palavra = lerPalavra(sc);
+        posicaoInicioPalavra(frase, palavra);
+        System.out.println("Posição de início da palavra no texto = " + posicaoInicioPalavra(frase, palavra));
         sc.close();
     }
 
